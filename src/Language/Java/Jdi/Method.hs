48a656e5dcf79af82284ad6c574378fb7100b0e5
Victor Denisov <denisovenator@gmail.com>
2013-01-19 13:21:55 +0400

    Functions of Method are moved to module Method.

--- src/Language/Java/Jdi/Method.hs
+++ src/Language/Java/Jdi/Method.hs
@@ -19,6 +19,7 @@ module Language.Java.Jdi.Method
 ) where
 
 import Language.Java.Jdi.Impl
+import Control.Monad (liftM)
 import Control.Monad.IO.Class (MonadIO)
 import Control.Monad.Error (ErrorT, runErrorT, MonadError(..), Error(..))
 import Data.Bits ((.&.))
@@ -57,3 +58,42 @@ location :: (Error e, MonadIO m, MonadError e m)
 location m@(Method ref method) = do
     (J.LineTable _ _ lines) <- receiveLineTable m
     return $ Location ref method (head lines)
+
+{- | Returns a list containing each LocalVariable that is declared as an
+argument of this method. If local variable information is not available, values
+of actual arguments to method invocations can be obtained by using the function
+getArgumentValues of StackFrame.
+
+Returns: the list of LocalVariable arguments. If there are no arguments,
+a zero-length list is returned.
+-}
+arguments :: (Error e, MonadIO m, MonadError e m) =>
+             Method -> VirtualMachine m [LocalVariable]
+arguments method = getVariables method (>)
+
+{- | Returns a list containing each LocalVariable declared in this method.
+The list includes any variable declared in any scope within the method. It may
+contain multiple variables of the same name declared within disjoint scopes.
+Arguments are considered local variables and will be present in the returned
+list. If local variable information is not available, values of actual arguments
+to method invocations can be obtained by using the function getArgumentValues
+of StackFrame.
+
+Returns: the list of LocalVariable objects which mirror local variables declared in
+this method in the target VM. If there are no local variables, a zero-length list is returned.
+-}
+variables :: (Error e, MonadIO m, MonadError e m) =>
+             Method -> VirtualMachine m [LocalVariable]
+variables method = getVariables method (<=)
+
+{- | Returns a list containing each LocalVariable of a given name in this
+method. Multiple variables can be returned if the same variable name is used in
+disjoint scopes within the method.
+
+Returns: the list of LocalVariable objects of the given name. If there are no
+matching local variables, a zero-length list is returned.
+-}
+variablesByName :: (Error e, MonadIO m, MonadError e m) =>
+                   Method -> String -> VirtualMachine m [LocalVariable]
+variablesByName method varName =
+    (filter ((varName ==) . localVariableName)) `liftM` (variables method)
